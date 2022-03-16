878bc8dafbc0494102be52eca51e9d91ae52eee3
Jaeyoung Kim <j.jaeykim@gmail.com>
2022-03-15 21:29:25 -0400

    add to cart capybara

--- Gemfile
+++ Gemfile
@@ -45,7 +45,9 @@ group :development, :test do
   gem 'byebug'
   gem 'quiet_assets'
   gem 'dotenv-rails', groups: [:development, :test]
-  
+  gem 'capybara'
+  gem 'poltergeist'
+  gem 'database_cleaner'
 end
 
 group :development do
---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+  scenario "Shows product details" do
+    # ACT
+    visit root_path
+    # save_screenshot
+    first('.product').click_link 'Details'
+    # DEBUG / VERIFY
+    
+    expect(page).to have_css '.product-detail'
+    # save_screenshot
+    
+  end
+end
